1be04f77508629b0ab6e36e2764f50e2880fc731
Guilherme M. Kogus <guilherme.mkogus@gmail.com>
2020-01-21 22:28:46 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,14 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello Git");
+		Position position = new Position(1, 2);
+		
+		System.out.println(position);
 
 	}
 
